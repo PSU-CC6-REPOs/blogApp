e26b52743398b70431f6db107bb6bb97820c4b23
Kristine Joy Martinez <kjvmartinez@Kristines-MacBook-Air-4.local>
2021-05-21 07:43:06 +0800

    modify config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-	define('ROOT_URL', 'http://localhost/phpsandbox/website8/');
-	define('DB_HOST', 'db4free.net');
-	define('DB_USER', 'bloguname');
-	define('DB_PASS', 'blogpassword');
-	define('DB_NAME', 'blogdemodb');
+	// define('ROOT_URL', 'http://localhost/phpsandbox/website8/');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6413807');
+	define('DB_PASS', 'y3ReZ1u1yT');
+	define('DB_NAME', 'sql6413807');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9ee8d8d3640298e4d4f642c0222fc91a969d6ad3',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9ee8d8d3640298e4d4f642c0222fc91a969d6ad3',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v1.13.2',
+      'version' => '1.13.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6d6a1009427d1f398c9d40904147bf9f723d5755',
+    ),
+  ),
+);
